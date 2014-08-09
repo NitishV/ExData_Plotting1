7fc6b3ca577d479115c5d6c385d82606deb17b4f
nitish <nitish.vashishtha@guavus.com>
2014-08-10 00:34:42 +0530

    Adding code and output for plot1 and plot2

---
+++ plot2.R
@@ -0,0 +1,5 @@
+house.power = read.table(file='../household_power_consumption.txt', header=T, sep=';', na.strings='?')
+house.power$Date_Time = as.POSIXct(paste(house.power[,1], house.power[,2]), format="%d/%m/%Y %H:%M:%S")
+house.power = house.power[,c(10,2:9)]
+house.power = subset(house.power, house.power$Date_Time > as.POSIXct('2007-02-01 00:00:00') & house.power$Date_Time <   as.POSIXct('2007-02-03 00:00:00'))
+plot(house.power$Date_Time, house.power$Global_active_power, xlab = '', ylab = 'Global Active Power (kilowatts)', type='l')
