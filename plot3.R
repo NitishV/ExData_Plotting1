8f8004ac115cf9ee546f6a18801b173d3266b0e6
nitish <nitish.vashishtha@guavus.com>
2014-08-11 00:20:21 +0530

    Adding code and output for plot 3 and 4

---
+++ plot3.R
@@ -0,0 +1,10 @@
+unzip(zipfile = 'exdata-data-household_power_consumption.zip')
+house.power = read.table(file='./household_power_consumption.txt', header=T, sep=';', na.strings='?')
+house.power$Date_Time = as.POSIXct(paste(house.power[,1], house.power[,2]), format="%d/%m/%Y %H:%M:%S")
+house.power = house.power[,c(10,2:9)]
+house.power = subset(house.power, house.power$Date_Time > as.POSIXct('2007-02-01 00:00:00') & house.power$Date_Time <   as.POSIXct('2007-02-03 00:00:00'))
+plot(house.power$Date_Time, house.power$Sub_metering_1, type = 'n', xlab='', ylab='Energy sub Metering', cex.lab=0.7)
+points(house.power$Date_Time, house.power$Sub_metering_1, type='l')
+points(house.power$Date_Time, house.power$Sub_metering_2, type='l', col='red')
+points(house.power$Date_Time, house.power$Sub_metering_3, type='l', col='blue')
+legend('topright', legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), col = c('black', 'red', 'blue'), lty=1, cex=0.8, text.width = 50000)
