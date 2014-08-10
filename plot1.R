54bf9c98172aee82218f7cf97230f88b22ea208f
nitish <nitish.vashishtha@guavus.com>
2014-08-11 00:57:02 +0530

    Updating plots for label sizing

--- plot1.R
+++ plot1.R
@@ -3,4 +3,4 @@ house.power = read.table(file='./household_power_consumption.txt', header=T, sep
 house.power$Date_Time = as.POSIXct(paste(house.power[,1], house.power[,2]), format="%d/%m/%Y %H:%M:%S")
 house.power = house.power[,c(10,2:9)]
 house.power = subset(house.power, house.power$Date_Time > as.POSIXct('2007-02-01 00:00:00') & house.power$Date_Time <   as.POSIXct('2007-02-03 00:00:00'))
-hist(x=house.power$Global_active_power, col = 'red', main = 'Global Active Power', xlab = 'Global Active Power (kilowatts)')
+hist(x=house.power$Global_active_power, col = 'red', main = 'Global Active Power', xlab = 'Global Active Power (kilowatts)', cex.lab=0.8)
--- plot2.R
+++ plot2.R
@@ -3,4 +3,4 @@ house.power = read.table(file='./household_power_consumption.txt', header=T, sep
 house.power$Date_Time = as.POSIXct(paste(house.power[,1], house.power[,2]), format="%d/%m/%Y %H:%M:%S")
 house.power = house.power[,c(10,2:9)]
 house.power = subset(house.power, house.power$Date_Time > as.POSIXct('2007-02-01 00:00:00') & house.power$Date_Time <   as.POSIXct('2007-02-03 00:00:00'))
-plot(house.power$Date_Time, house.power$Global_active_power, xlab = '', ylab = 'Global Active Power (kilowatts)', type='l')
+plot(house.power$Date_Time, house.power$Global_active_power, xlab = '', ylab = 'Global Active Power (kilowatts)', type='l', cex.lab=0.8)
