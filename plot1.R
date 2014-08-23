94e6b4200a1a7c43a8af5369ad375c6614a416a1
nitish <nitish.vashishtha@guavus.com>
2014-08-23 12:37:45 +0530

    Updating for review comments on project

--- plot1.R
+++ plot1.R
@@ -1,8 +1,7 @@
-unzip(zipfile = 'exdata-data-household_power_consumption.zip')
 house.power = read.table(file='./household_power_consumption.txt', header=T, sep=';', na.strings='?')
 house.power$Date_Time = as.POSIXct(paste(house.power[,1], house.power[,2]), format="%d/%m/%Y %H:%M:%S")
 house.power = house.power[,c(10,2:9)]
 house.power = subset(house.power, house.power$Date_Time > as.POSIXct('2007-02-01 00:00:00') & house.power$Date_Time <   as.POSIXct('2007-02-03 00:00:00'))
+png(filename = 'plot1.png', width = 480, height = 480)
 hist(x=house.power$Global_active_power, col = 'red', main = 'Global Active Power', xlab = 'Global Active Power (kilowatts)', cex.lab=0.8)
-dev.copy(png, file='plot1.png')
 dev.off()
--- plot2.R
+++ plot2.R
@@ -3,6 +3,6 @@ house.power = read.table(file='./household_power_consumption.txt', header=T, sep
 house.power$Date_Time = as.POSIXct(paste(house.power[,1], house.power[,2]), format="%d/%m/%Y %H:%M:%S")
 house.power = house.power[,c(10,2:9)]
 house.power = subset(house.power, house.power$Date_Time > as.POSIXct('2007-02-01 00:00:00') & house.power$Date_Time <   as.POSIXct('2007-02-03 00:00:00'))
+png(filename = 'plot1.png', width = 480, height = 480)
 plot(house.power$Date_Time, house.power$Global_active_power, xlab = '', ylab = 'Global Active Power (kilowatts)', type='l', cex.lab=0.8)
-dev.copy(png, file='plot2.png')
 dev.off()
